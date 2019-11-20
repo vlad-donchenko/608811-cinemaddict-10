ecc5472be2aa2771ca110d93831e91b17362fc67
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-11-20 23:22:30 +0200

    module1-task3 - Отрисовал Карточку фильма, Меню, Кнопка и Попап

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const FILM_COUNT = 5;
+const FILM_COUNT_EXTRA = 2;
+
 const createCardTemplate = () => {
   return (
     `
@@ -227,56 +230,27 @@ const createShowMoreButtonTemplate = () => {
   );
 };
 
-const createUserLevelTemplate = () => {
-  return (
-    `
-    <section class="statistic">
-      <form action="https://echo.htmlacademy.ru/" method="get" class="statistic__filters">
-        <p class="statistic__filters-description">Show stats:</p>
-  
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" checked="">
-        <label for="statistic-all-time" class="statistic__filters-label">All time</label>
-  
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today">
-        <label for="statistic-today" class="statistic__filters-label">Today</label>
-  
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week">
-        <label for="statistic-week" class="statistic__filters-label">Week</label>
-  
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month" value="month">
-        <label for="statistic-month" class="statistic__filters-label">Month</label>
-  
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year">
-        <label for="statistic-year" class="statistic__filters-label">Year</label>
-      </form>
-  
-      <ul class="statistic__text-list">
-        <li class="statistic__text-item">
-          <h4 class="statistic__item-title">You watched</h4>
-          <p class="statistic__item-text">0 <span class="statistic__item-description">movies</span></p>
-        </li>
-        <li class="statistic__text-item">
-          <h4 class="statistic__item-title">Total duration</h4>
-          <p class="statistic__item-text">0<span class="statistic__item-description">h</span> 0<span class="statistic__item-description">m</span></p>
-        </li>
-        <li class="statistic__text-item">
-          <h4 class="statistic__item-title">Top genre</h4>
-          <p class="statistic__item-text">undefined</p>
-        </li>
-      </ul>
-  
-      <div class="statistic__chart-wrap">
-        <canvas class="statistic__chart" width="1000"></canvas>
-      </div>
-  
-    </section>
-    `
-  );
-};
-
 const renderTemplate = (wrapper, template, position = `beforeend`) => {
-  wrapper.insertAdjacentElement(position, template);
+  wrapper.insertAdjacentHTML(position, template);
 };
 
+const footer = document.querySelector(`.footer`);
 const siteMain = document.querySelector(`.main`);
-renderTemplate(siteMain, createNavigationTemplate());
+const cardWrapper = siteMain.querySelector(`.films-list .films-list__container`);
+const cardsExtraWrapper = siteMain.querySelectorAll(`.films-list--extra .films-list__container`);
+
+renderTemplate(siteMain, createNavigationTemplate(), `afterbegin`);
+
+new Array(FILM_COUNT)
+  .fill(``)
+  .forEach(() => renderTemplate(cardWrapper, createCardTemplate()));
+
+Array.from(cardsExtraWrapper).forEach((container) => {
+  new Array(FILM_COUNT_EXTRA)
+    .fill(``)
+    .forEach(() => renderTemplate(container, createCardTemplate()));
+});
+
+renderTemplate(cardWrapper, createShowMoreButtonTemplate(), `afterend`);
+renderTemplate(footer, createModalTemplate(), `afterend`);
+
