e3d199d26d8da7e9c4abd63e9062aa731c871e94
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-11-24 23:07:16 +0200

    module2-task1 - Добавляет сборку проекта и разбивку на модули

---
+++ src/components/show-more-button.js
@@ -0,0 +1,9 @@
+const createShowMoreButtonTemplate = () => {
+  return (
+    `
+    <button class="films-list__show-more">Show more</button>
+    `
+  );
+};
+
+export {createShowMoreButtonTemplate};
