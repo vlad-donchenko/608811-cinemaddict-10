e1075a85b046fc4cb3f9d8940ffcf5b6efef88ab
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-12-22 21:13:08 +0200

    module4-task1 - Перепишет на класс Show More Button

--- src/components/show-more-button.js
+++ src/components/show-more-button.js
@@ -1,3 +1,5 @@
+import {createElement} from '../utils.js';
+
 const createShowMoreButtonTemplate = () => {
   return (
     `
@@ -5,5 +7,27 @@ const createShowMoreButtonTemplate = () => {
     `
   );
 };
+
+class ShowMoreButton {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createShowMoreButtonTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
 
 export {createShowMoreButtonTemplate};
