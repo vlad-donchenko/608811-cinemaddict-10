0cf2cac61af42eddf0ca6386094af642c3b04314
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-01-03 13:31:43 +0200

    module4-task2 - Заменяет if/else - на тернатрин

--- src/components/film-list.js
+++ src/components/film-list.js
@@ -1,24 +1,23 @@
 import {createElement} from '../utils.js';
 
 const createFilmsTemplate = (filmsAmount) => {
-  if (!filmsAmount) {
-    return (
-      `<section class="films">
+  const templateWithoutFilms = (
+    `<section class="films">
         <section class="films-list">
            <h2 class="films-list__title">There are no movies in our database</h2>
         </section>
     </section>`
-    );
-  } else {
-    return (
-      `<section class="films">
+  );
+
+  const templateWithFilms = (
+    `<section class="films">
         <section class="films-list">
             <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
             <div class="films-list__container"></div>
-        </section>
     </section>`
-    );
-  }
+  );
+
+  return (filmsAmount) ? templateWithFilms : templateWithoutFilms;
 };
 
 export default class FilmList {
