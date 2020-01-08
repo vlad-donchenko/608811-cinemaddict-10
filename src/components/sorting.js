acaffe5f5883496e1999724f06732f855edf5de7
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-01-08 17:57:02 +0200

    module5-task2 - Добавляет типы сортировки и обработчик в классе

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,11 +1,17 @@
 import AbstractComponent from "./abstract";
 
+const SortType = {
+  SORT_DEFAULT: `sort-default`,
+  SORT_DATE: `sort-date`,
+  SORT_RATING: `sort-rating`,
+};
+
 const createSortTemplate = () => {
   return (
     `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      <li><a href="#" class="sort__button sort__button--active" data-sort="${SortType.SORT_DEFAULT}">Sort by default</a></li>
+      <li><a href="#" class="sort__button" data-sort="${SortType.SORT_DATE}">Sort by date</a></li>
+      <li><a href="#" class="sort__button" data-sort="${SortType.SORT_RATING}">Sort by rating</a></li>
     </ul>`
   );
 };
@@ -17,5 +23,9 @@ export default class Sort extends AbstractComponent {
 
   getTemplate() {
     return createSortTemplate();
+  }
+
+  setSortChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, handler);
   }
 }
