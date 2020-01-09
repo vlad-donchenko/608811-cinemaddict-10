6452b99e8a6ac1ed74d94a8532ad701553077403
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-01-09 18:53:21 +0200

    module5-task2 - Рефакторинг

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
-import MenuComponent from './components/menu';
-import SortComponent from './components/sorting';
 import UserRankComponent from './components/user-rank';
-import FilmListComponent from './components/film-list';
 import PageController from './controllers/page';
 import {films, getRandomIntegerNumber} from './mock/film';
-import {menuFilmCount} from './mock/filter';
 import {render, RenderPosition} from './utils/render';
 
 const footerFilmsCount = document.querySelector(`.footer__statistics`);
@@ -15,15 +11,6 @@ const userRankComponent = new UserRankComponent(getRandomIntegerNumber(1, 30));
 render(siteHeader, userRankComponent, RenderPosition.BEFOREEND);
 
 const siteMain = document.querySelector(`.main`);
-const menuComponent = new MenuComponent(menuFilmCount(films));
-const sortComponent = new SortComponent();
-render(siteMain, menuComponent, RenderPosition.BEFOREEND);
-render(siteMain, sortComponent, RenderPosition.BEFOREEND);
-
-const filmListComponent = new FilmListComponent(films.length);
-render(siteMain, filmListComponent, RenderPosition.BEFOREEND);
-
-const filmsWrapper = siteMain.querySelector(`.films`);
-const pageController = new PageController(filmsWrapper);
+const pageController = new PageController(siteMain);
 pageController.render(films);
 
